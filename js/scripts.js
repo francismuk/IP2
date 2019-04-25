375b702a4dcdb66249e67bb3bfef2280860f082f
Francis Mukuha <mukuha58@gmail.com>
2019-04-25 16:50:39 +0300

    add if-else in the js file

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,13 @@ var add= function(side1, side2, side3) {
 var side1 =prompt("Insert first side:");
 var side2 =prompt("Insert second side:");
 var side3 =prompt("Insert third side:")
+
+if (side1==side2 && side1==side3) {
+  alert("Equilateral triangle")
+} else if (side1==side2 || side1==side3 || side2==side3) {
+  alert("Isosceles triangle")
+} else if (side1!==side2 && side1!==side3 && side2!==side3) {
+  alert("Scalene")
+} else if (add(side1+side2)==side3 && add(side2+side3)==side1 && add(side1+side3)==side2) {
+  alert("A triangle CANNOT be formed!")
+}
