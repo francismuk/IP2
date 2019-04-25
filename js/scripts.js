6fb8c1ec726d5a9481fac575f4d2c9c057084cbb
Francis Mukuha <mukuha58@gmail.com>
2019-04-25 16:47:24 +0300

    added prompts to add variables

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var add= function(side1, side2, side3) {
+  return side1 + side2 + side3
+}
+var side1 =prompt("Insert first side:");
+var side2 =prompt("Insert second side:");
+var side3 =prompt("Insert third side:")
