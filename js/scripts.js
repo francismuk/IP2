33ebd94e302b2aa82ac2a654c3a084424aa1d81c
Francis Mukuha <mukuha58@gmail.com>
2019-04-26 11:13:39 +0300

    add line breakers and get results button

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
-var add= function(side1, side2, side3) {
-  return side1 + side2 + side3
-}
+function results() {
 var side1 =prompt("Insert first side:");
 var side2 =prompt("Insert second side:");
 var side3 =prompt("Insert third side:");
+
 
 if (side1==side2 && side1==side3) {
   alert("Equilateral triangle")
@@ -13,4 +12,5 @@ if (side1==side2 && side1==side3) {
   alert("Scalene")
 } else if (add(side1+side2)==side3 && add(side2+side3)==side1 && add(side1+side3)==side2) {
   alert("A triangle CANNOT be formed!")
+}
 }
