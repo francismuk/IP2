d99214dfb46ccc52b776ead7f89343eac88177a9
Francis Mukuha <mukuha58@gmail.com>
2019-04-26 17:21:32 +0300

    add an unwritten readme file

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@ function results() {
 var side1 =prompt("Insert first side:");
 var side2 =prompt("Insert second side:");
 var side3 =prompt("Insert third side:");
-
+var sum1 = side1 + side2;
+var sum2 = side1 + side3;
+var sum3 = side2 + side3;
 
 if (side1==side2 && side1==side3) {
   alert("Equilateral triangle")
@@ -10,10 +12,7 @@ if (side1==side2 && side1==side3) {
   alert("Isosceles triangle")
 } else if (side1!==side2 && side1!==side3 && side2!==side3) {
   alert("Scalene")
-} else if (add(side1+side2)==side3 && add(side2+side3)==side1 && add(side1+side3)==side2) {
+} else if ( sum1<=side3 && sum2<=side2 && sum3<=side1) {
   alert("A triangle CANNOT be formed!")
 }
 }
-<script>
-<button onclick="location.reload();">Refresh Page</button>
-</script>
